7eca80f41f684e0a8528be6257a5b2a5ecfeb452
Giuseppe Sacco <giuseppesacco146@gmail.com>
2021-10-13 20:12:17 +0200

    Added comments to INI

--- ADR.py
+++ ADR.py
@@ -4,46 +4,15 @@ import random, os
 import numpy as np
 from timeit import default_timer as timer
 import imageio, cv2
-from configparser import ConfigParser
 
 import custom_plots     # Custom library with plotting functions
+from init import *      # Parsing all parameters and general infos from ini file 'parameters.ini' through 'init.py'
 
 # TO DO:
 # Check conservation laws
 # Vectorise loops
 
 timer_start = timer()
-
-# Parsing all parameters and general infos from ini file 'parameters.ini'
-# For details on the following definitions, check the ini file.
-cfg = ConfigParser()
-cfg.read('parameters.ini')
-
-Nx = cfg.getint('simulation params', 'Nx')
-Ny = cfg.getint('simulation params', 'Ny')
-Nt = cfg.getint('simulation params', 'Nt')
-N_period = cfg.getint('simulation params', 'N_period')
-dt = cfg.getfloat('simulation params', 'dt')
-alpha_x0 = cfg.getfloat('simulation params', 'alpha_x0')
-d_alpha_x = cfg.getfloat('simulation params', 'd_alpha_x')
-delta = cfg.getfloat('simulation params', 'delta')
-b = cfg.getfloat('simulation params', 'b')
-a_chem_m = cfg.getfloat('simulation params', 'a_chem_m')
-a_chem_range = cfg.getfloat('simulation params', 'a_chem_range')
-c_m = cfg.getfloat('simulation params', 'c_m')
-c_range = cfg.getfloat('simulation params', 'c_range')
-savefig_dir = cfg.get('files and directories', 'savefig_dir').format(alpha_x0,d_alpha_x,N_period)
-dnk1 = cfg.getint('plotting params', 'dnk1')
-save_c_avg = cfg.getboolean('saving', 'save_c_avg')
-save_c_nowind_avg = cfg.getboolean('saving', 'save_c_nowind_avg')
-gifname = cfg.get('saving', 'gifname').format(alpha_x0,d_alpha_x,N_period)
-fname_c_avg = cfg.get('saving','fname_c_avg')
-fname_c_nowind_avg = cfg.get('saving','fname_c_nowind_avg')
-show_3Dplot = cfg.getboolean('plotting params', 'show_3Dplot')
-save_array_dir = cfg.get('saving', 'save_array_dir')
-second_plot_name = cfg.get('saving', 'second_plot_name').format(alpha_x0,d_alpha_x,N_period)
-rand_seed = cfg.getint('simulation params', 'rand_seed')
-make_first_plot = cfg.getboolean('plotting params', 'make_first_plot')
 
 print(f'alpha_0 = {alpha_x0}, d_alpha = {d_alpha_x}, omega = 2pi/{N_period}')
 
