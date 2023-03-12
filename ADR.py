9fd6a8fab03c4b4f34120440bcdad7e05e9fc6ef
giusSacco <giuseppesacco146@gmail.com>
2023-03-13 00:11:58 +0100

    Added param to choose if compute const_wind and FT

--- ADR.py
+++ ADR.py
@@ -33,7 +33,7 @@ def ADR(*, Nx: int, Ny : int, Nt : int, N_period : int,
         save_c_avg : bool, save_c_constwind_avg : bool, show_3Dplot : bool, save_array_dir : str,
         second_plot_name : str, rand_seed : int, make_first_plot:bool, 
         N_t_stationary_min : int, stationary_start : bool, 
-        stat_pops_dir : str, stationary_c_fname : str, stationary_c_constwind_fname : str):
+        stat_pops_dir : str, stationary_c_fname : str, stationary_c_constwind_fname : str, do_const_wind : bool, do_FT : bool):
 
     timer_start = timer()
 
@@ -65,23 +65,27 @@ def ADR(*, Nx: int, Ny : int, Nt : int, N_period : int,
     stationary_initial_cond = os.path.exists(stationary_c_fname) and os.path.exists(stationary_c_constwind_fname) and stationary_start
     if stationary_initial_cond:    # If stationary files are present and user chose stationary intial cond.
         c = np.load(stationary_c_fname)
-        c_constwind = np.load(stationary_c_constwind_fname)
+        if do_const_wind:
+            c_constwind = np.load(stationary_c_constwind_fname)
     else:
         c = np.random.uniform(low = c_m - c_range, high = c_m+c_range, size = (Ny,Nx))
-        c_constwind=c.copy()
+        if do_const_wind:
+            c_constwind=c.copy()
         Nt += N_t_stationary # If files are not present, we create it first and than proceed normally when stationariety is reached
 
-    # Initialise matrices
-    fft_section_0 = np.zeros((Nt,Nx))       # Section for k_y = 0 of the |FT| in k_x,t plane
-    fft_section_1 = np.zeros((Nt,Nx))       # Sect. for n_k_y = dnk1
+    if do_FT:
+        # Initialise matrices
+        fft_section_0 = np.zeros((Nt,Nx))       # Section for k_y = 0 of the |FT| in k_x,t plane
+        fft_section_1 = np.zeros((Nt,Nx))       # Sect. for n_k_y = dnk1
 
-    fft_section_0_constwind = np.zeros((Nt,Nx))    # Same as above but for the constwind sys.
-    fft_section_1_constwind = np.zeros((Nt,Nx))
+        fft_section_0_constwind = np.zeros((Nt,Nx))    # Same as above but for the constwind sys.
+        fft_section_1_constwind = np.zeros((Nt,Nx))
     filenames= []
 
 
     c_avg = []      # Average population for each t
-    c_constwind_avg = []
+    if do_const_wind:
+        c_constwind_avg = []
 
     # Time evolution: SWSS
     for nt in range(Nt):
@@ -93,54 +97,57 @@ def ADR(*, Nx: int, Ny : int, Nt : int, N_period : int,
         c = 0.5 * (AD_propagator_vector(c_chem, alpha_x, delta) + Chem_propagator_vector(c_ad, a_chem, b, dt))
 
         # Same as above but for CONSTANT WIND
-        alpha_x = alpha_x0      
+        if do_const_wind:
+            alpha_x = alpha_x0      
 
-        c_ad = AD_propagator_vector(c_constwind, alpha_x, delta)
-        c_chem = Chem_propagator_vector(c_constwind, a_chem, b, dt)
-        c_constwind = 0.5 * (AD_propagator_vector(c_chem, alpha_x, delta) + Chem_propagator_vector(c_ad, a_chem, b, dt))
+            c_ad = AD_propagator_vector(c_constwind, alpha_x, delta)
+            c_chem = Chem_propagator_vector(c_constwind, a_chem, b, dt)
+            c_constwind = 0.5 * (AD_propagator_vector(c_chem, alpha_x, delta) + Chem_propagator_vector(c_ad, a_chem, b, dt))
         
         # Saving stationary arrays if we didn't start from stationariety
         if nt == N_t_stationary and not stationary_initial_cond: 
-            np.save(stationary_c_fname, c) # Note that one of them can be already existing ad is overwritten 
-            np.save(stationary_c_constwind_fname, c_constwind)
+            np.save(stationary_c_fname, c) # Note that one of them can be already existing ad is overwritten
+            if do_const_wind:
+                np.save(stationary_c_constwind_fname, c_constwind)
         
         # Update average population
         if nt >= N_t_stationary or not stationary_start: # If user didn't request stationary start we save also the transient population
             c_avg.append(c.mean())
-            c_constwind_avg.append(c_constwind.mean())            
+            if do_const_wind:
+                c_constwind_avg.append(c_constwind.mean())            
         
-        
-        # Fourier transform of population
-        fft=np.fft.fft2(c)      
-        # Placing origin at the center...
-        x1,x2, x3, x4= fft[:Ny//2,:Nx//2], fft[Ny//2:,:Nx//2], fft[:Ny//2,Nx//2:], fft[Ny//2:,Nx//2:]
-        fft1=np.concatenate((x3, x1), axis=1)
-        fft2=np.concatenate((x4, x2), axis=1)
-        fft=np.concatenate((fft2, fft1), axis=0)
+        if do_FT:
+            # Fourier transform of population
+            fft=np.fft.fft2(c)      
+            # Placing origin at the center...
+            x1,x2, x3, x4= fft[:Ny//2,:Nx//2], fft[Ny//2:,:Nx//2], fft[:Ny//2,Nx//2:], fft[Ny//2:,Nx//2:]
+            fft1=np.concatenate((x3, x1), axis=1)
+            fft2=np.concatenate((x4, x2), axis=1)
+            fft=np.concatenate((fft2, fft1), axis=0)
 
-        fft0=np.fft.fft2(c_constwind)
-        x1,x2, x3, x4= fft0[:Ny//2,:Nx//2], fft0[Ny//2:,:Nx//2], fft0[:Ny//2,Nx//2:], fft0[Ny//2:,Nx//2:]
-        fft1=np.concatenate((x3, x1), axis=1)
-        fft2=np.concatenate((x4, x2), axis=1)
-        fft0=np.concatenate((fft2, fft1), axis=0)
+            fft0=np.fft.fft2(c_constwind)
+            x1,x2, x3, x4= fft0[:Ny//2,:Nx//2], fft0[Ny//2:,:Nx//2], fft0[:Ny//2,Nx//2:], fft0[Ny//2:,Nx//2:]
+            fft1=np.concatenate((x3, x1), axis=1)
+            fft2=np.concatenate((x4, x2), axis=1)
+            fft0=np.concatenate((fft2, fft1), axis=0)
 
-        # Averaging along k_y in order to reduce noise
-        fft_section_0[nt][:] = np.mean(np.abs(fft[Ny//2 - 1:Ny//2 +1][:]), axis = 0)
-        fft_section_1[nt][:] = np.mean(np.abs(fft[Ny//2 + dnk1 - 1:Ny//2 + dnk1 +1][:]), axis = 0)
+            # Averaging along k_y in order to reduce noise
+            fft_section_0[nt][:] = np.mean(np.abs(fft[Ny//2 - 1:Ny//2 +1][:]), axis = 0)
+            fft_section_1[nt][:] = np.mean(np.abs(fft[Ny//2 + dnk1 - 1:Ny//2 + dnk1 +1][:]), axis = 0)
 
-        fft_section_0_constwind[nt][:] = np.mean(np.abs(fft0[Ny//2 - 1:Ny//2 +1][:]), axis = 0)
-        fft_section_1_constwind[nt][:] = np.mean(np.abs(fft0[Ny//2 + dnk1 - 1:Ny//2 + dnk1 +1][:]), axis = 0)
-        #fft_section_0[nt][:] = scipy.signal.savgol_filter(tmp, 5, 3)
-        
-        # PLOT of c, |FT[c]|, c_0, |FT[c_0]|, (c-c_0)/c_0, |FT[c]|/|FT[c_0]|
-        if make_first_plot and nt % 5 == 0:
-            kwargs = {'c':c,'vmax':c_m+c_range, 'vmin':c_m-c_range ,'fft':fft,'c_constwind':c_constwind, 'fft0':fft0,\
-                'alpha_x0':alpha_x0,'d_alpha_x':d_alpha_x, 'omega':omega,'dt':dt,'Nt':Nt,'nt':nt,\
-                'filename' : f'{nt}.png', 'savefig_dir':savefig_dir}
-            
-            custom_plots.FirstPlot(**kwargs)
+            fft_section_0_constwind[nt][:] = np.mean(np.abs(fft0[Ny//2 - 1:Ny//2 +1][:]), axis = 0)
+            fft_section_1_constwind[nt][:] = np.mean(np.abs(fft0[Ny//2 + dnk1 - 1:Ny//2 + dnk1 +1][:]), axis = 0)
+            #fft_section_0[nt][:] = scipy.signal.savgol_filter(tmp, 5, 3)
 
-            filenames.append(kwargs['filename'])
+            # PLOT of c, |FT[c]|, c_0, |FT[c_0]|, (c-c_0)/c_0, |FT[c]|/|FT[c_0]|
+            if make_first_plot and nt % 5 == 0:
+                kwargs = {'c':c,'vmax':c_m+c_range, 'vmin':c_m-c_range ,'fft':fft,'c_constwind':c_constwind, 'fft0':fft0,\
+                    'alpha_x0':alpha_x0,'d_alpha_x':d_alpha_x, 'omega':omega,'dt':dt,'Nt':Nt,'nt':nt,\
+                    'filename' : f'{nt}.png', 'savefig_dir':savefig_dir}
+
+                custom_plots.FirstPlot(**kwargs)
+
+                filenames.append(kwargs['filename'])
         if nt % 100 == 0:    
             print(nt)
 
@@ -151,42 +158,45 @@ def ADR(*, Nx: int, Ny : int, Nt : int, N_period : int,
             os.mkdir(save_array_dir)
         if save_c_avg:
             np.save( os.path.join(save_array_dir,fname_c_avg.format(N_period)) , c_avg)
-        if save_c_constwind_avg:
+        if save_c_constwind_avg and do_const_wind:
             np.save( os.path.join(save_array_dir,fname_c_constwind_avg.format(alpha_x0)) , c_constwind_avg)
 
     # Create gif with previouses images
-    if make_first_plot:
+    if make_first_plot and do_FT:
         with imageio.get_writer(os.path.join(savefig_dir,gifname), mode='I', fps = 2) as writer:
             for filename in filenames:
                 image = imageio.imread(os.path.join(savefig_dir,filename))
                 writer.append_data(image)
 
 
+    
     #%%   
-    # Gaussain blur and log scale for fft
-    dk1 = (2*np.pi/Ny)*dnk1     # Circa pi/4
+    if do_FT:
+        # Gaussain blur and log scale for fft
+        dk1 = (2*np.pi/Ny)*dnk1     # Circa pi/4
 
-    Z_0 = np.log10(fft_section_0[:][:].transpose())
-    Z_1 = np.log10(fft_section_1[:][:].transpose())
-    Z_0_blur = cv2.GaussianBlur(Z_0, (7, 7), 0)
-    Z_1_blur = cv2.GaussianBlur(Z_1, (7, 7), 0)
+        Z_0 = np.log10(fft_section_0[:][:].transpose())
+        Z_1 = np.log10(fft_section_1[:][:].transpose())
+        Z_0_blur = cv2.GaussianBlur(Z_0, (7, 7), 0)
+        Z_1_blur = cv2.GaussianBlur(Z_1, (7, 7), 0)
 
-    Z_0_constwind = np.log10(fft_section_0_constwind[:][:].transpose())
-    Z_1_constwind = np.log10(fft_section_1_constwind[:][:].transpose())
-    Z_0_blur_constwind = cv2.GaussianBlur(Z_0_constwind, (7, 7), 0)
-    Z_1_blur_constwind = cv2.GaussianBlur(Z_1_constwind, (7, 7), 0)
+        Z_0_constwind = np.log10(fft_section_0_constwind[:][:].transpose())
+        Z_1_constwind = np.log10(fft_section_1_constwind[:][:].transpose())
+        Z_0_blur_constwind = cv2.GaussianBlur(Z_0_constwind, (7, 7), 0)
+        Z_1_blur_constwind = cv2.GaussianBlur(Z_1_constwind, (7, 7), 0)
 
-    # 3D PLOT of fft_section_0
-    if show_3Dplot:
-        custom_plots.ThreeD_Plot(Nt=Nt, Nx=Nx, Z_0=Z_0)
+        # 3D PLOT of fft_section_0
+        if show_3Dplot:
+            custom_plots.ThreeD_Plot(Nt=Nt, Nx=Nx, Z_0=Z_0)
 
     #%%
-    # PLOT on k_x,t plane of |FT[c]|, |FT[c_0]|, |FT[c]|/|FT[c_0]|
-    kwargs2 = {'Nt':Nt, 'N_period':N_period, 'Z_0_blur':Z_0_blur, \
-        'Z_1_blur':Z_1_blur, 'alpha_x0':alpha_x0, 'd_alpha_x':d_alpha_x,\
-        'omega':omega, 'dt':dt, 'Z_0_blur_constwind':Z_0_blur_constwind, \
-        'Z_1_blur_constwind':Z_1_blur_constwind, 'second_plot_name':second_plot_name, 'savefig_dir' : savefig_dir}
-    custom_plots.SecondPlot(**kwargs2)
+    if do_FT:
+        # PLOT on k_x,t plane of |FT[c]|, |FT[c_0]|, |FT[c]|/|FT[c_0]|
+        kwargs2 = {'Nt':Nt, 'N_period':N_period, 'Z_0_blur':Z_0_blur, \
+            'Z_1_blur':Z_1_blur, 'alpha_x0':alpha_x0, 'd_alpha_x':d_alpha_x,\
+            'omega':omega, 'dt':dt, 'Z_0_blur_constwind':Z_0_blur_constwind, \
+            'Z_1_blur_constwind':Z_1_blur_constwind, 'second_plot_name':second_plot_name, 'savefig_dir' : savefig_dir}
+        custom_plots.SecondPlot(**kwargs2)
 
     # Print total time elapsed
     print(f'Time elapsed: {timer()-timer_start:.1f}s')
